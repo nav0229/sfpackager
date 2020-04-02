af96c474b6c60e81f2f1197854ca194f91a26d3b
Nav <nsingh@acumensolutions.com>
2020-04-02 02:47:50 -0400

    Update git_import_sfpackager_diff.sh

--- git_import_sfpackager_diff.sh
+++ git_import_sfpackager_diff.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/sh
-TODAY=`date '+%m_%d_%Y'.%H.%M.%S`
 ORGname=AIE
 staging="dev_sandbox"
 GitRepo="sfpackager"
 URL="https://github.com/nav0229/sfpackager.git"
 branch="dev1"
-tagname="$ORGname"_"$staging"_import_"$TODAY"
-echo " Tag name to be created is = $tagname"
-echo "-------"
-echo " 1) ---Retrieve metadata from Staging Sandbox"
-#rm -fR retrieveUnpackaged
-#ant retrieveUnpackaged
-	#cd "$GitRepo"
+
 	git checkout "$branch"
-	cp -R ../retrieveUnpackaged/* ./src/
+	cp -R ./retrieveUnpackaged/* ./src/
 	git add ./src/*
-	git commit -m "$tagname"
+	git commit -m "checkin"
 	git push
 
 echo " 3) ---Starting calculation diff between UAT1 and team branch ----"

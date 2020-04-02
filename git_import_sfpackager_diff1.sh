51d33aeb785b2bbaceb532d8a23a636e6dc40784
Nav <nsingh@acumensolutions.com>
2020-04-02 02:59:29 -0400

    Update git_import_sfpackager_diff1.sh

--- git_import_sfpackager_diff1.sh
+++ git_import_sfpackager_diff1.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/sh
+1#!/usr/bin/sh
 ORGname=AIE
 staging="dev_sandbox"
 GitRepo="sfpackager"
 URL="https://github.com/nav0229/sfpackager.git"
-branch="dev1"
 
-	git checkout "$branch"
+	git checkout dev1
 	cp -R ./retrieveUnpackaged/* ./src/
 	git add ./src/*
 	git commit -m "checkin"
@@ -15,6 +14,6 @@ echo " 3) ---Starting calculation diff between UAT1 and team branch ----"
 export PATH=$PATH:C:/SF_PACKAGER/full_packager/sf-packager/bin
 export PATH=$PATH:C:/SF_PACKAGER/full_packager/sf-packager
 git checkout uat1
-git checkout "$branch"
-C:/SF_PACKAGER/full_packager/sf-packager/index.js uat1 "$branch" ./pkg_diff_"$TODAY"
+git checkout dev1
+C:/SF_PACKAGER/full_packager/sf-packager/index.js uat1 dev1 ./pkg_diff
 

14692f72307a7ad6c92300b0782bd84a40198e07
Nav <nsingh@acumensolutions.com>
2020-04-02 09:43:57 -0400

    Update git_import_sfpackager_diff1.sh

--- git_import_sfpackager_diff1.sh
+++ git_import_sfpackager_diff1.sh
@@ -1,10 +1,10 @@
 1#!/usr/bin/sh
 git pull origin
-cp -R ./retrieveUnpackaged/* ./src/
+git branch -a
+cp  ./retrieveUnpackaged/* ./src/
 git add ./src/*
 git commit -m "checkin"
 git push
-
 echo " 3) ---Starting calculation diff between UAT1 and team branch ----"
 #export PATH=$PATH:C:/SF_PACKAGER/full_packager/sf-packager/bin
 #export PATH=$PATH:C:/SF_PACKAGER/full_packager/sf-packager

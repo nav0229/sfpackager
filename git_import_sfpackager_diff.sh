9791fc28596397272715428daedbf26477fd54b9
Nav <nsingh@acumensolutions.com>
2020-04-02 02:44:23 -0400

    Update git_import_sfpackager_diff.sh

--- git_import_sfpackager_diff.sh
+++ git_import_sfpackager_diff.sh
@@ -1,34 +1,16 @@
 #!/usr/bin/sh
-#Title	:This script works in 3 steps
-# a- retrieve sandbox metadta
-# b- commit metadata into branch
-# c- Invoke sfpackager and create the package
-#
-#author		 	 :Navdeep Singh
-#date            :04012018
-#version         :0.1  
-#usage		     :git import + invoke sfpackager to DIFF
-
 TODAY=`date '+%m_%d_%Y'.%H.%M.%S`
 ORGname=AIE
 staging="dev_sandbox"
-GitRepo="pr-071472"
+GitRepo="sfpackager"
 URL="https://github.com/nav0229/sfpackager.git"
 branch="dev1"
-#Export Variables
-export TODAY
-export staging
-export GitRepo
-export URL
-export tagname
-export ORGname
-echo "Todays Date is = $TODAY "
 tagname="$ORGname"_"$staging"_import_"$TODAY"
 echo " Tag name to be created is = $tagname"
 echo "-------"
 echo " 1) ---Retrieve metadata from Staging Sandbox"
-rm -fR retrieveUnpackaged
-ant retrieveUnpackaged
+#rm -fR retrieveUnpackaged
+#ant retrieveUnpackaged
 	#cd "$GitRepo"
 	git checkout "$branch"
 	cp -R ../retrieveUnpackaged/* ./src/
